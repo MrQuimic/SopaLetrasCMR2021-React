1ba3d85bc1fc3a15c2d2310734f159d6d426b6b9
CarlosSantos <100442327+MrQuimic@users.noreply.github.com>
2024-01-25 14:41:45 +0000

    Update footer.component.jsx

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>@ D E I S  - 21/22 - Carlos Santos</p>
+      <p>@ D E I S - Carlos Santos - 21/22 </p>
       
     </footer>
   );
