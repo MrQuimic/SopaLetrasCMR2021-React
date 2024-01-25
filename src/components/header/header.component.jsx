f2f2dfa126af2c29a7b12eae828d5920ef844589
CarlosSantos <100442327+MrQuimic@users.noreply.github.com>
2024-01-25 14:46:34 +0000

    Update header.component.jsx
    Header update

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h2 className="tittle">Sopa de letras</h2>
-      <h4 className="subtittle">ISEC - Linguagens Script</h4>
+      <h4 className="subtittle">Jogo - Linguagens Script</h4>
 
       {/* <h4 className="subtittle">React 21/22</h4>*/}
     </header>
